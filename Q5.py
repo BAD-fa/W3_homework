a0a0e174ff5836de8b3ea1eab8a46e65a0800bef
BAD <fathibehrad@gmail.com>
2021-11-13 03:06:41 +0330

    Question 5 solved

---
+++ Q5.py
@@ -0,0 +1,15 @@
+def blue_red(numbers: list, colors: list):
+    colored_number = sorted(zip(colors, numbers))
+    # print(colored_number)
+    for i, (c, n) in enumerate(colored_number, 1):
+        if c == 'B' and i > n or c == 'R' and i < n:
+            return 'NO'
+    return 'YES'
+
+
+t = int(input())
+for j in range(t):
+    l = int(input())
+    numb = list(map(int, input().split(' ')))
+    color = input()
+    print(blue_red(numb, color))
